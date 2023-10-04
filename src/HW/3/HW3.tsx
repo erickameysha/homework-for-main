7c0c8d0ef116ecc0ae5012cca9ce3399dd1f24d1
ekameyshaaa <ekameyshaaa@gmail.com>
2023-10-04 20:15:52 +0300

    Hw3

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -10,7 +10,7 @@ export const HW3 = () => {
   // - приложение должно компилироваться и запускаться в браузере
 
 
-  const [currentText, setCurrentText] = useState('');
+  const [currentText, setCurrentText] = useState<string>('');
   const [texts, setTexts] = useState<string[]>([
     'То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)',
   ]);
@@ -29,7 +29,7 @@ setTexts([currentText,...texts])
   return (
     <div id={'hw03'}>
       {currentText ? (
-        <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
+        <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
         <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
       )}
